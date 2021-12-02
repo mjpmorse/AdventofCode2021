c0c1df5b443c94524dea995addf3cca07bb5555a
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2021-12-02 09:50:28 -0500

    Day 2 Complete

---
+++ day02/d2q1.py
@@ -0,0 +1,24 @@
+import numpy as np
+from icecream import ic
+
+with open('day02/data_q2.txt', 'r') as f:
+    data = np.loadtxt(f, dtype=str)
+    directions = data[:, 1].astype(int)
+    command = data[:, 0]
+
+if False:
+    data = np.array([['forward', 5],
+                     ['down', 5],
+                     ['forward', 8],
+                     ['up', 3],
+                     ['down', 8],
+                     ['forward', 2]])
+    directions = data[:, 1].astype(int)
+    command = data[:, 0]
+
+down = np.sum(directions[command == 'down']) - \
+       np.sum(directions[command == 'up'])
+forward = np.sum(directions[command == 'forward'])
+
+print(f"move down {down} units and forward {forward} units")
+print(f"The product is {down * forward}")
