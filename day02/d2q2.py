fc04e6c85c73ffaac624c6d0b2c5a92d04c6c8c5
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2021-12-02 10:03:02 -0500

    changed it up a bit

--- day02/d2q2.py
+++ day02/d2q2.py
@@ -1,35 +1,19 @@
-import numpy as np
-from icecream import ic
-
-with open('day02/data_q2.txt', 'r') as f:
-    data = np.loadtxt(f, dtype=str)
-    directions = data[:, 1].astype(int)
-    command = data[:, 0]
-
-if False:
-    data = np.array([['forward', 5],
-                     ['down', 5],
-                     ['forward', 8],
-                     ['up', 3],
-                     ['down', 8],
-                     ['forward', 2]])
-    directions = data[:, 1].astype(int)
-    command = data[:, 0]
-
 aim = 0
 depth = 0
 position = 0
 
-for command in data:
-    if command[0] == 'down':
-        aim += command[1].astype(int)
+with open('day02/data_q2.txt', 'r') as f:
+    for line in f:
+        command = line.split(' ')
+        if command[0] == 'down':
+            aim += int(command[1])
 
-    elif command[0] == 'up':
-        aim -= command[1].astype(int)
+        elif command[0] == 'up':
+            aim -= int(command[1])
 
-    elif command[0] == 'forward':
-        position += command[1].astype(int)
-        depth += aim * command[1].astype(int)
+        elif command[0] == 'forward':
+            position += int(command[1])
+            depth += aim * int(command[1])
 
 print(f"move down {depth} units and forward {position} units")
 print(f"The product is {depth * position}")
