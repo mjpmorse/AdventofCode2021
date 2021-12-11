1137ea3cf2b354c18a3c83ae6fbb093e50203896
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2021-12-11 05:55:20 -0500

    animate Q9

--- src/day09/day9q2.py
+++ src/day09/day9q2.py
@@ -1,7 +1,30 @@
 from icecream import ic
 from typing import List, Tuple
+from time import sleep
 debug = False
+animate = False
 heightMap = []
+
+# This sets the WIDTH and HEIGHT of each grid location
+width = 10
+height = 10
+margin = 0
+
+# Define some colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+lava = (204, 102, 0)
+
+if animate:
+    # put the import here because the package is large
+    import pygame
+    pygame.init()
+    pygame.display.set_caption("")
+
+    clock = pygame.time.Clock()
 
 file = 'data_q9_dummy.txt' if debug else 'data_q9.txt'
 with open(f'data/{file}', 'r') as f:
@@ -15,6 +38,10 @@ for row in heightMap:
     basinMap.append([int(not x // 9) for x in row])
 
 ic(basinMap) if debug else ""
+
+if animate:
+    windowSize = [height * len(basinMap[0]), width * len(basinMap)]
+    screen = pygame.display.set_mode(windowSize)
 
 
 def comparePoints(
@@ -49,17 +76,41 @@ def comparePoints(
     return check
 
 
+def updateScreen(basinMap):
+    for row in range(len(basinMap)):
+        for column in range(len(basinMap[0])):
+            if basinMap[row][column] == 0:
+                color = black
+            elif basinMap[row][column] == -1:
+                color = lava
+            else:
+                color = white
+            pygame.draw.rect(
+                screen,
+                color,
+                [(margin + width) * column + margin,
+                    (margin + height) * row + margin,
+                    width,
+                    height]
+                )
+    clock.tick(60)
+    pygame.display.flip()
+    sleep(0.001)
+
+
 def zerosAround(basinMap, row, column):
     count = 0
     mapDepth = len(basinMap) - 1
     mapWidth = len(basinMap[0]) - 1
 
-    if basinMap[row][column] == 0:
+    if basinMap[row][column] != 1:
         return count
 
     else:
         count += 1
-        basinMap[row][column] = 0
+        basinMap[row][column] = -1
+        if animate:
+            updateScreen(basinMap)
 
     # if we are not on top, check above us
     if row != 0:
@@ -88,6 +139,9 @@ basinList = []
 maxBasinNumber = len(basinMap) * len(basinMap[0])
 startingX = 0
 startingY = 0
+
+if animate:
+    updateScreen(basinMap)
 while any([1 in row for row in basinMap]):
     iterations += 1
     # find any value of 1 to start
@@ -111,3 +165,6 @@ statement = (
     f"The product of the three largest basins is: {product}"
 )
 print(statement)
+
+if animate:
+    pygame.quit()
