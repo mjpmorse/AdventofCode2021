539956ff7aaeee633d82eaf8a74ab4c57cd4b724
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2021-12-15 16:31:37 -0500

    just tinkering now

--- src/day15/day15.py
+++ src/day15/day15.py
@@ -34,6 +34,38 @@ class CaveNode:
         return hash(repr(self))
 
 
+def neighbors(caveMap, node) -> CaveNode:
+
+    np = getattr(node, 'position')
+    ml = len(caveMap) - 1
+
+    if np[0] > 0:
+        yield CaveNode(
+                node,
+                (np[0] - 1, np[1]),
+                caveMap[np[0] - 1][np[1]]
+                )
+    if np[0] < ml:
+        yield CaveNode(
+                node,
+                (np[0] + 1, np[1]),
+                caveMap[np[0] + 1][np[1]]
+                )
+
+    if np[1] > 0:
+        yield CaveNode(
+                node,
+                (np[0], np[1] - 1),
+                caveMap[np[0]][np[1] - 1]
+                )
+    if np[1] < ml:
+        yield CaveNode(
+                node,
+                (np[0], np[1] + 1),
+                caveMap[np[0]][np[1] + 1]
+                )
+
+
 def findCavePath(caveMap, start, end):
 
     startNode = CaveNode(None, start, caveMap[start[0]][start[1]])
@@ -65,49 +97,22 @@ def findCavePath(caveMap, start, end):
                 current = getattr(current, 'parent')
             return path[::-1]
 
-        children = []
-        for newPosition in [(1, 0), (0, -1), (0, 1), (-1, 0)]:
-
-            # Get node position
-            nodePosition = (
-                getattr(currentNode, 'position')[0] + newPosition[0],
-                getattr(currentNode, 'position')[1] + newPosition[1]
-                )
-
-            # Make sure within range
-            condition = (
-                nodePosition[0] > (len(caveMap) - 1) or
-                nodePosition[0] < 0 or
-                nodePosition[1] > (len(caveMap[-1]) - 1) or
-                nodePosition[1] < 0
-            )
-            if condition:
-                continue
-
-            # Create new node
-            newNode = CaveNode(
-                currentNode,
-                nodePosition,
-                caveMap[nodePosition[0]][nodePosition[1]]
-                )
-
-            # Append
-            children.append(newNode)
-
         # Loop through children
-        for child in children:
+        for child in neighbors(caveMap, currentNode):
             # Child is on the closed list
             if child in closedList:
                 continue
 
             # Create the f, g, and h values
             setattr(child, 'g', getattr(currentNode, 'g') + child.get_cost())
+            setattr(child, 'h', 0)
             setattr(child, 'f', getattr(child, 'g') + getattr(child, 'h'))
             # Child is already in the open list
             if child in openList:
                 position = openList.index(child)
                 if openList[position].g > child.g:
-                    openList[position] = child
+                    openList.pop(position)
+                    openList.append(child)
                 continue
             # Add the child to the open list
             openList.append(child)
